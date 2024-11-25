a8ceb3d45c6b6dc62427239237a273e1caa422df
TJOEUNHY <lee@>
2024-11-25 18:16:35 +0900

    둘다 리스트로 입력받고 for문으로 변경

--- src/main/java/numberbaseball/Application.java
+++ src/main/java/numberbaseball/Application.java
@@ -10,10 +10,10 @@ public class Application {
 
         Random random = new Random();
         for (int i = 1; i < 10; i++) {
-                num.add(i);
+            num.add(i);
         }
         Collections.shuffle(num);
-        List<Integer> randomNum = num.subList(0,3);
+        List<Integer> randomNum = num.subList(0, 3);
         System.out.println(randomNum);
 
 
@@ -23,53 +23,57 @@ public class Application {
 
         while (true) {
             // 사용자 입력 받기 (이 부분은 완성되어 있음)
+            ArrayList<Integer> user = new ArrayList<>();
             System.out.print("\n첫 번째 숫자를 입력하세요: ");
-            int user1 = scanner.nextInt();
+            user.add(scanner.nextInt());
             System.out.print("두 번째 숫자를 입력하세요: ");
-            int user2 = scanner.nextInt();
+            user.add(scanner.nextInt());
             System.out.print("세 번째 숫자를 입력하세요: ");
-            int user3 = scanner.nextInt();
+            user.add(scanner.nextInt());
 
-            int strike =0;
+            int strike = 0;
+            int ball = 0;
             // TODO: strike 개수를 계산하세요
-            if(randomNum.get(0)==user1){
-                strike =strike+1;
+            for (int i = 0; i < 3; i++) {
+                if (randomNum.get(i) == user.get(i)) {
+                    strike = strike + 1;
+                } else if (randomNum.get(i) == user.get(i)) {
+                    for (int j = 0; j < 3; j++) {
+                        ball = ball + 1;
+                    }
+
+
+                }
             }
-            if(randomNum.get(1)==user2){
-                strike =strike+1;
-            }
-            if(randomNum.get(2)==user3){
-                strike =strike+1;
-            }
-            int ball =0;
-            if(randomNum.get(0)==user2){
-                ball =ball+1;
-            }
-            if(randomNum.get(0)==user3){
-                ball =ball+1;
-            }
-            if(randomNum.get(1)==user1){
-                ball =ball+1;
-            }
-            if(randomNum.get(1)==user3){
-                ball =ball+1;
-            }
-            if(randomNum.get(2)==user1){
-                ball =ball+1;
-            }
-            if(randomNum.get(2)==user2){
-                ball =ball+1;
-            }
+//
+//            if(randomNum.get(0)==user.get(1)){
+//                ball =ball+1;
+//            }
+//            if(randomNum.get(0)==user.get(2)){
+//                ball =ball+1;
+//            }
+//            if(randomNum.get(1)==user.get(0)){
+//                ball =ball+1;
+//            }
+//            if(randomNum.get(1)==user.get(2)){
+//                ball =ball+1;
+//            }
+//            if(randomNum.get(2)==user.get(0)){
+//                ball =ball+1;
+//            }
+//            if(randomNum.get(2)==user.get(1)){
+//                ball =ball+1;
+//            }
             // TODO: 결과를 출력하세요 (예: "1 스트라이크")
             System.out.println(strike + " 스트라이크");
             System.out.println(ball + "  볼");
 
             // TODO: 3 스트라이크인 경우 게임을 끝내세요
-            if(strike==3) break;
+            if (strike == 3) break;
 
         }
 
-         System.out.println("축하합니다! 정답을 맞추셨습니다.");
+        System.out.println("축하합니다! 정답을 맞추셨습니다.");
     }
 
 }
