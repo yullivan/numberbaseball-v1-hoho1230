2ff4d0a7984998ab3c07b068adc02e0dbd023f95
TJOEUNHY <lee@>
2024-12-05 16:30:25 +0900

    enum 생성 후 사용

--- src/main/java/numberbaseball/Ball.java
+++ src/main/java/numberbaseball/Ball.java
@@ -11,25 +11,20 @@ public class Ball {
         this.position = position;
         this.number = number;
     }
-    String matchStatus(Ball userBall) {
-       int strike=0;
-       int ball=0;
+
+
+    Enum<BallStatus> matchStatus(Ball userBall) {
 
         if (position == userBall.position) {
             if (number == userBall.number) {
-                strike =strike+1;
-                System.out.println("strike = " + strike);
-                return  "STRIKE";
+                return BallStatus.STRIKE;
             }
-
         } else if (number == userBall.number) {
-            ball =ball +1;
-            System.out.println("ball = " + ball);
-            return  "BALL";
-        } else
-        System.out.println("아웃");
-        return  "NOTHING";
+            return BallStatus.BALL;
+        }
+            return BallStatus.NOTHING;
     }
+
 
 
 }
