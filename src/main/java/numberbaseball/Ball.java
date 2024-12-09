1af84538cb169b6d87b6c76cab94ae890f0ea064
TJOEUNHY <lee@>
2024-12-09 13:30:03 +0900

    1209 private 사용함에 따른 getter setter 만들기 ,object끼리 비교하기위한 equals 함수 만들기

--- src/main/java/numberbaseball/Ball.java
+++ src/main/java/numberbaseball/Ball.java
@@ -2,11 +2,12 @@ package numberbaseball;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Ball {
-    int position;
-    int number;
-    List<Ball> balls;
+   private int position;
+    private int number;
+    private List<Ball> balls;
 
     public Ball(int position, int number) {
         this.position = position;
@@ -16,11 +17,10 @@ public class Ball {
 
     Enum<BallStatus> matchStatus(Ball userBall) {
 
-        if (position == userBall.position) {
-            if (number == userBall.number) {
+        if (balls.equals(userBall)) {
                 return BallStatus.STRIKE;
-            }
-        } else if (number == userBall.number) {
+
+        } else if (position == userBall.position) {
             return BallStatus.BALL;
         }
             return BallStatus.NOTHING;
@@ -34,6 +34,40 @@ public class Ball {
         return BallStatus.NOTHING;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Ball ball = (Ball) o;
+        return position == ball.position && number == ball.number && Objects.equals(balls, ball.balls);
+    }
 
+    public int getPosition() {
+        return position;
+    }
 
+    public void setPosition(int position) {
+        this.position = position;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public List<Ball> getBalls() {
+        return balls;
+    }
+
+    public void setBalls(List<Ball> balls) {
+        this.balls = balls;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(position, number, balls);
+    }
 }
