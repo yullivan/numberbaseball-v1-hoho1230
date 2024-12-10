5c4d7f3dc0ad5d6f5243d27dc80ebd9c7cc594a9
TJOEUNHY <lee@>
2024-12-10 16:32:18 +0900

    1210 3단계 list와 list 비교 기능구현,  게임 결과 클래스에 볼과스트라이크 카운트하는 기능 구현

--- src/main/java/numberbaseball/Balls.java
+++ src/main/java/numberbaseball/Balls.java
@@ -1,26 +1,42 @@
 package numberbaseball;
 
-import java.awt.*;
 import java.util.List;
-import java.util.Objects;
 
 public class Balls {
     //private Ball ball;
     private List<Ball> balls;
 
-    BallStatus matchStatus(Ball userBall) {
+    BallStatus matchStatus1(Ball userBall) {
         for (Ball ball : balls) {
             BallStatus result = ball.matchStatus(userBall);
-              if(result==BallStatus.STRIKE){
-                  return result;
-              }else if(result==BallStatus.BALL){
-                  return result;
-              }
+            if (result == BallStatus.STRIKE) {
+                return result;
+            } else if (result == BallStatus.BALL) {
+                return result;
+            }
         }
         return BallStatus.NOTHING;
     }
+
+
+    GameResult matchStatus2(Balls userBalls) {
+        GameResult result = new GameResult(0, 0);
+        for (Ball ball : balls) {
+            result.incrementCount(userBalls.matchStatus1(ball));
+
+        }
+        return result;
+    }
+
     public Balls(List<Ball> balls) {
         this.balls = balls;
+    }
+
+    @Override
+    public String toString() {
+        return "Balls{" +
+                "balls=" + balls +
+                '}';
     }
 }
 
--- src/test/java/numberbaseball/ApplicationTests.java
+++ src/test/java/numberbaseball/ApplicationTests.java
@@ -1,7 +1,6 @@
 package numberbaseball;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -43,12 +42,38 @@ class ApplicationTests {
                 new Ball(1, 2),new Ball(1, 3)));
 
 
-        System.out.println(balls);
+       // System.out.println(balls);
         //when
 
         //then
-        assertThat(balls.matchStatus(userBall1)).isEqualTo(BallStatus.STRIKE);
-        assertThat(balls.matchStatus(userBall2)).isEqualTo(BallStatus.BALL);
-        assertThat(balls.matchStatus(userBall3)).isEqualTo(BallStatus.NOTHING);
+        assertThat(balls.matchStatus1(userBall1)).isEqualTo(BallStatus.STRIKE);
+        assertThat(balls.matchStatus1(userBall2)).isEqualTo(BallStatus.BALL);
+        assertThat(balls.matchStatus1(userBall3)).isEqualTo(BallStatus.NOTHING);
+    }
+
+    @Test
+    void name3() {
+        //given
+        Balls balls= new Balls(List.of(new Ball(1, 1),
+                new Ball(2, 2),new Ball(3, 3)));
+
+        Balls userBalls1= new Balls(List.of(new Ball(1, 1),
+                new Ball(2, 2),new Ball(3, 3)));
+        Balls userBalls2= new Balls(List.of(new Ball(1, 3),
+                new Ball(2, 2),new Ball(3, 1)));
+        Balls userBalls3= new Balls(List.of(new Ball(1, 3),
+                new Ball(2, 9),new Ball(3, 1)));
+        Balls userBalls4= new Balls(List.of(new Ball(1, 4),
+                new Ball(2, 5),new Ball(3, 6)));
+        //when
+
+        //then
+       // System.out.println(balls.matchStatus2(userBalls2));
+
+        assertThat(balls.matchStatus2(userBalls1)).isEqualTo(new GameResult(3,0));
+        assertThat(balls.matchStatus2(userBalls2)).isEqualTo(new GameResult(1,2));
+        assertThat(balls.matchStatus2(userBalls3)).isEqualTo(new GameResult(0,2));
+        assertThat(balls.matchStatus2(userBalls4)).isEqualTo(new GameResult(0,0));
+
     }
 }
